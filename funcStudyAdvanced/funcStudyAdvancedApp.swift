177407e5338e3c6bfdd44977a25a201b8f03a2de
Wongi <hpurs95@gmail.com>
2022-07-17 14:26:38 +0900

    Initial Commit

---
+++ funcStudyAdvanced/funcStudyAdvancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  funcStudyAdvancedApp.swift
+//  funcStudyAdvanced
+//
+//  Created by MacBook Air on 2022/07/17.
+//
+
+import SwiftUI
+
+@main
+struct funcStudyAdvancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
